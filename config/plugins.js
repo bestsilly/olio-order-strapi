74ab93d7071b1b22a22b85d100cdf7d2ccecbbf8
bestsilly <anuwut.kon@gmail.com>
2025-06-15 20:47:02 +0700

    feat: configure graphql plugin with custom settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      landingPage: true, // enable Sandbox everywhere
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true
+      },
+    },
+  },
+});
